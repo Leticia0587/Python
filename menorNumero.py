643d052b8d2577202ee683b72324b4e2402f8c02
unknown <leticiadearaujo005@gmail.com>
2022-08-29 16:33:09 -0300

    exercicios em python

---
+++ menorNumero.py
@@ -0,0 +1,6 @@
+numero_um = int(input("Insira um valor inteiro: "))
+numero_dois = int(input("Insira o segundo valor inteiro: "))
+
+resultado = numero_um if numero_um < numero_dois else numero_dois
+
+print("O menor número é", resultado)
