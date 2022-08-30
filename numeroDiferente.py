e26b696b6a58e3e0905b9d34a2ca640bee682555
unknown <leticiadearaujo005@gmail.com>
2022-08-30 11:51:10 -0300

    exercicio em python

---
+++ numeroDiferente.py
@@ -0,0 +1,6 @@
+numero_um = int(input("Insira um valor inteiro: "))
+numero_dois = int(input("Insira o segundo valor inteiro: "))
+
+resultado = 'igual' if numero_um == numero_dois else 'diferente'
+
+print("O número é", resultado)
