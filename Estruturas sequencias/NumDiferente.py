2cacc523b5c5e6d359440c3717e67fd76f0877c2
unknown <leticiadearaujo005@gmail.com>
2022-09-02 20:37:41 -0300

    exercicios python

--- Estruturas sequencias/NumDiferente.py
+++ Estruturas sequencias/NumDiferente.py
@@ -1,6 +1,6 @@
 numero_um = int(input("Insira um valor inteiro: "))
 numero_dois = int(input("Insira o segundo valor inteiro: "))
-
-resultado = 'igual' if numero_um == numero_dois else 'diferente'
-
-print("O número é", resultado)
+if numero_um == numero_dois:
+    print("Os números são iguais!")
+else:
+    print("Os números são diferentes!")
